a3139a87fb3308687ef285d6f8f03faf40b274c5
ganavijayaram <jayaram.g@northeastern.edu>
2023-07-31 18:54:57 -0400

    Identifying the part which causes multiple requests on every refresh

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import MovieList from './MovieList'
 
 //using the arrow function syntax in JavaScript.
 //Functional components are a fundamental building block in React, and they are used to create reusable UI elements.
+
+//when you refresh, all the posts have to be fetched, then we need to get their relevant comments
 const App = () => {
     return <div className='container'>
         <h1>Create Movie</h1>
